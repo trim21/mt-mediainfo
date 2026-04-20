fc40de36b25ba3c6ce12a4e43403298a54ed4576
Trim21 <trim21.me@gmail.com>
2026-04-20 22:00:19 +0800

    update

--- app/config.py
+++ app/config.py
@@ -29,9 +29,6 @@ def _data_dir() -> Path:
 
 
 def default_node_id() -> str:
-    if node_id := os.getenv("NODE_ID"):
-        return node_id
-
     data_dir = _data_dir()
     node_id_file = data_dir.joinpath("node_id")
 
@@ -46,37 +43,37 @@ def default_node_id() -> str:
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class BaseConfig:
-    debug: Annotated[bool, Field(os.getenv("DEBUG") or False, validate_default=False)]
+    debug: Annotated[bool, Field(alias="DEBUG", default=False, validate_default=False)]
 
-    pg_host: Annotated[str, Field(os.environ.get("PG_HOST", "127.0.0.1"), validate_default=True)]
-    pg_port: Annotated[int, Field(os.environ.get("PG_PORT", "5432"), validate_default=True)]
-    pg_db: Annotated[str, Field(os.environ.get("PG_DB", "postgres"))]
+    pg_host: Annotated[str, Field(alias="PG_HOST", default="127.0.0.1", validate_default=True)]
+    pg_port: Annotated[int, Field(alias="PG_PORT", default="5432", validate_default=True)]
+    pg_db: Annotated[str, Field(alias="PG_DB", default="postgres")]
     pg_user: Annotated[
-        str | None, Field(os.environ.get("PG_USER", "postgres"), validate_default=True)
+        str | None, Field(alias="PG_USER", default="postgres", validate_default=True)
     ]
     pg_password: Annotated[
-        str | None, None, Field(os.environ.get("PG_PASSWORD", "postgres"), validate_default=True)
+        str | None, None, Field(alias="PG_PASSWORD", default="postgres", validate_default=True)
     ]
 
     pg_sslmode: Annotated[
         str | None,
         BeforeValidator(lambda x: x or None),
-        Field(os.environ.get("PG_SSLMODE")),
+        Field(alias="PG_SSLMODE", default=None),
     ]
     pg_ssl_rootcert: Annotated[
         str | None,
         BeforeValidator(lambda x: x or None),
-        Field(os.environ.get("PG_SSL_ROOTCERT")),
+        Field(alias="PG_SSL_ROOTCERT", default=None),
     ]
     pg_ssl_cert: Annotated[
         str | None,
         BeforeValidator(lambda x: x or None),
-        Field(os.environ.get("PG_SSL_CERT")),
+        Field(alias="PG_SSL_CERT", default=None),
     ]
     pg_ssl_key: Annotated[
         str | None,
         BeforeValidator(lambda x: x or None),
-        Field(os.environ.get("PG_SSL_KEY")),
+        Field(alias="PG_SSL_KEY", default=None),
     ]
 
     def pg_dsn(self) -> str:
@@ -109,28 +106,28 @@ class BaseConfig:
 class S3Mixin:
     s3_bucket: Annotated[
         str,
-        Field(min_length=1, default_factory=lambda: os.environ["S3_BUCKET"]),
+        Field(alias="S3_BUCKET", min_length=1),
     ]
     s3_region: Annotated[
         str,
-        Field(min_length=1, default_factory=lambda: os.environ["S3_REGION"]),
+        Field(alias="S3_REGION", min_length=1),
     ]
     s3_endpoint: Annotated[
         str,
-        Field(min_length=1, default_factory=lambda: os.environ["S3_ENDPOINT"]),
+        Field(alias="S3_ENDPOINT", min_length=1),
     ]
     s3_access_key_id: Annotated[
         str,
-        Field(min_length=1, default_factory=lambda: os.environ["S3_ACCESS_KEY_ID"]),
+        Field(alias="S3_ACCESS_KEY_ID", min_length=1),
     ]
     s3_secret_access_key: Annotated[
         str,
-        Field(min_length=1, default_factory=lambda: os.environ["S3_SECRET_ACCESS_KEY"]),
+        Field(alias="S3_SECRET_ACCESS_KEY", min_length=1),
     ]
     s3_root: Annotated[
         str | None,
         BeforeValidator(lambda x: x or None),
-        Field(os.environ.get("S3_ROOT")),
+        Field(alias="S3_ROOT", default=None),
     ]
 
 
@@ -139,7 +136,7 @@ class NodeConfig(BaseConfig, S3Mixin):
     node_id: Annotated[
         str,
         Field(
-            alias="node-id",
+            alias="NODE_ID",
             min_length=1,
             default_factory=default_node_id,
         ),
@@ -148,43 +145,42 @@ class NodeConfig(BaseConfig, S3Mixin):
     qb_url: Annotated[
         HttpUrl | None,
         BeforeValidator(lambda x: x or None),
-        Field(os.environ.get("QB_URL"), validate_default=True),
+        Field(alias="QB_URL", default=None, validate_default=True),
     ]
 
     download_path: Annotated[
         str,
         Field(
-            os.environ.get("DOWNLOAD_PATH", os.path.expanduser("~/downloads")),
+            alias="DOWNLOAD_PATH",
+            default=os.path.expanduser("~/downloads"),
             validate_default=True,
         ),
     ]
 
     total_process_size: Annotated[
         ByteSize,
-        Field(os.environ.get("TOTAL_SIZE", "100GiB"), validate_default=True),
+        Field(alias="TOTAL_SIZE", default="100GiB", validate_default=True),
     ]
 
     single_torrent_size_limit: Annotated[
         ByteSize,
-        Field(os.environ.get("SINGLE_TORRENT_SIZE_LIMIT", "10GiB"), validate_default=True),
+        Field(alias="SINGLE_TORRENT_SIZE_LIMIT", default="10GiB", validate_default=True),
     ]
 
     pick_strategy: Annotated[
         PickStrategy,
-        Field(os.environ.get("PICK_STRATEGY", PickStrategy.seeders), validate_default=True),
+        Field(alias="PICK_STRATEGY", default=PickStrategy.seeders, validate_default=True),
     ]
 
     version: Annotated[
         str,
-        Field(os.environ.get("APP_VERSION", ""), validate_default=True),
+        Field(alias="APP_VERSION", default="", validate_default=True),
     ]
 
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class ScrapeConfig(BaseConfig, S3Mixin):
-    mt_token: Annotated[
-        str, Field(min_length=1, default_factory=lambda: os.environ["MT_API_TOKEN"])
-    ]
+    mt_token: Annotated[str, Field(alias="MT_API_TOKEN", min_length=1)]
 
     # filter empty string
     http_proxy: Annotated[str | None, BeforeValidator(lambda x: x or None)] = None
@@ -196,15 +192,15 @@ class ServerConfig(BaseConfig):
 
 
 def load_node_config() -> NodeConfig:
-    return parse_obj(NodeConfig, {})
+    return parse_obj(NodeConfig, dict(os.environ))
 
 
 def load_scrape_config() -> ScrapeConfig:
-    return parse_obj(ScrapeConfig, {})
+    return parse_obj(ScrapeConfig, dict(os.environ))
 
 
 def load_server_config() -> ServerConfig:
-    return parse_obj(ServerConfig, {})
+    return parse_obj(ServerConfig, dict(os.environ))
 
 
 def _copy_key_with_permissions(src: str) -> str:
